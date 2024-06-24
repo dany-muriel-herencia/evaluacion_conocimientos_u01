b37c85337f9080e760c5cf6d0cdff8a3e27cd795
dany-muriel-herencia <drmurielh@unjbg.edu.pe>
2024-06-23 20:56:47 -0500

    Actualizacion del codigo dos

--- ejercicio 2 ec.cpp
+++ ejercicio 2 ec.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
 using namespace std;
 
-bool mcd (int, int); 
+bool mcd (int, int); //pónemos algunas funciones
 
 
 int main(){
 
 bool primos;
 int n;  
-cout<<"introduce el numero"<<endl;
+cout<<"introduce el numero"<<endl;//introduce bel numero que quieres analizar
 cin>>n;
 
 for(int m=3;m<n ;m++){ 
 
 primos=mcd(n,m);
-if (primos)
+if (primos)//usamos el if para saber su estado ,si nes primo entre si o no 
 cout << n << " y " << m << " son primos entre si\n";
 else 
 cout << n << " y " << m << " no son primos entre si\n";
